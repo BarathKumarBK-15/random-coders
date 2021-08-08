b459164a677b3ee2e18ee80a7823f72d3e0dfeb0
BarathKumar <barathganeshkumar2002@gmail.com>
2021-08-08 16:58:02 +0530

    initial commit

---
+++ js/rc.js
@@ -0,0 +1,21 @@
+function gotosol(){
+	window.location.href = "Solutions.html";
+}
+function gotochap(n){
+	window.location.href = "Chap"+n+".html";
+}
+function gotopc(){
+	window.location.href = "q.html"
+}
+function gotops(){
+	window.location.href = "ps.html"
+}
+function prob(n){
+	window.location.href = "#"+n;
+}
+function gotochapq(n){
+	window.location.href = "Chap"+n+"q.html";
+}
+function gotochaps(n){
+	window.location.href = "Chap"+n+"p.html";
+}
